0296d47395181905061a9f023400972397de1245
MangoTsing <757271842@qq.com>
2020-11-27 18:52:37 +0800

    fix blockConsole bug

--- src/shatter_error/eventHandle.ts
+++ src/shatter_error/eventHandle.ts
@@ -74,8 +74,8 @@ export const BindStaticEvent = function (w: eventWarp, options: BlockEventSingle
             const fileArr = fileMsg.split(':')
             const line = fileArr[fileArr.length - 2]
             const col = fileArr[fileArr.length - 1]
-            const url = fileMsg.slice(0, -line.length -col.length - 2)
-            const msg = event.reason.message;
+            const url = fileMsg && fileMsg.slice(0, -line.length -col.length - 2)
+            const msg = event.reason.message
             w.report({
                 name: ERRORNAMETYPES['promiseError'], msg, url, line, col, type: ERRORTYPES['PROMISE_ERROR']
             })
@@ -95,7 +95,7 @@ export const BindStaticEvent = function (w: eventWarp, options: BlockEventSingle
                         const fileArr = fileMsg.split(':')
                         const line = fileArr[fileArr.length - 2]
                         const col = fileArr[fileArr.length - 2]
-                        const url = (fileMsg.split('(') && fileMsg.split('(')[1].slice(0, -line.length -col.length - 2)) || 'anonymous function'
+                        const url = (fileMsg.split('(')[1] && fileMsg.split('(')[1].slice(0, -line.length -col.length - 2)) || 'anonymous function'
                         w.report({
                             name: ERRORNAMETYPES['consoleError'], msg: item.stack, url, line, col, type: ERRORTYPES['LOG_ERROR']
                         })
